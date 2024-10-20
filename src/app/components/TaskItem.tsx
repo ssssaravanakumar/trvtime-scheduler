1d7b76d3b708dc373045d3e728cc5e2ff12e4126
cb-saravanakumarn <saravanakumar.n@chargebee.com>
2024-10-20 13:17:34 +0530

    styles added

--- src/app/components/TaskItem.tsx
+++ src/app/components/TaskItem.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TaskItem = ({ task, isDragging, connectDragSource, connectDragPreview }) => {
   console.log(task);
-  const dragContent = <li className='p-3 border-l-4 border-blue-500 bg-blue-100 rounded-md shadow-md transition-all duration-300 transform hover:scale-105 hover:shadow-lg'>{task.state.title}</li>;
+  const dragContent = <li className='h-[22px] w-[400px] text-[#ffffff] bg-[rgb(128,197,246)] rounded-md flex items-center px-2 shadow-md transition-all duration-300 transform hover:scale-105 hover:shadow-lg'>{task.state.title}</li>;
 
   return isDragging ? null : <>{connectDragPreview(connectDragSource(dragContent))}</>;
 }
